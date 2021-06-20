fe4d2a7c86977f7cca3e0201c244157c5560b73b
Jason <tsai301103@gmail.com>
2021-06-20 15:52:30 +0800

    Initial Commit

---
+++ test_git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test_git
+//
+//  Created by 蔡孟典 on 2021/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test_git/test_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_gitApp.swift
+//  test_git
+//
+//  Created by 蔡孟典 on 2021/6/20.
+//
+
+import SwiftUI
+
+@main
+struct test_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
